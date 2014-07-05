1b6776a5676d35a179dd8de0cdc907f33a9bcaea
Keijiro Takahashi <keijiro@Super-MSX.local>
2014-07-05 15:53:49 +0900

    Added the project contents.

---
+++ Assets/Gold.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gold : MonoBehaviour
+{
+    public GameObject clearText;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            clearText.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector3.up * 300);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(Vector3.right * Input.GetAxis("Horizontal") * 20);
+        rigidbody.AddForce(Vector3.forward * Input.GetAxis("Vertical") * 20);
+    }
+}
